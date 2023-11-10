73fa643e0030ffdf9a6e3904cc216ce78dde4192
Rumbustious <99288580+Rumbustious@users.noreply.github.com>
2023-11-10 15:50:57 +0200

    Basic Validation ⚙️ - Added all fields regx - Added instant typing check Validation

--- src/Form.jsx
+++ src/Form.jsx
@@ -1,6 +1,5 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import apelLogo from "./assets/apel.svg";
-
 function App() {
   // state of the form's data
   const [formData, setFormData] = useState({
@@ -13,15 +12,84 @@ function App() {
     university: "",
     academicYear: "",
   });
-  const [validationErrors, setValidationErrors] = useState({});
+  const [focusedFields, setFocusedFields] = useState([]);
+  const [validationErrors, setValidationErrors] = useState({
+    teamName: false,
+    arName: false,
+    enName: false,
+    email: false,
+    phoneNumber: false,
+    nationalId: false,
+    university: false,
+    academicYear: false,
+  });
+
+  // checkvalidation
+  function preValidate(formData) {
+    const teamNameRegx = /\w|^[ء-ي ]*$/;
+    const arNameRegx = /[ء-ي ] [ء-ي ]/g;
+    const enNameRegx = /[a-zA-Z ] [a-zA-Z ]/g;
+    const emailRegx = /\w@(gmail|yahoo).com/g;
+    const phoneRegx = /01(0|1|2|5)\d{8}/g;
+    const idRegx = /(2|3)\d{6}28\d{5}/g;
+    setValidationErrors({
+      teamName: false,
+      arName: false,
+      enName: false,
+      email: false,
+      phoneNumber: false,
+      nationalId: false,
+      university: false,
+      academicYear: false,
+    });
+    if (!teamNameRegx.test(formData.teamName)) {
+      console.log("invalid team name");
+      setValidationErrors((prevErrorState) => {
+        return { ...prevErrorState, teamName: true };
+      });
+    }
+    if (!arNameRegx.test(formData.arName)) {
+      console.log("invalid arabic name");
+      setValidationErrors((prevErrorState) => {
+        return { ...prevErrorState, arName: true };
+      });
+    }
+    if (!enNameRegx.test(formData.enName)) {
+      console.log("invalid english name");
+      setValidationErrors((prevErrorState) => {
+        return { ...prevErrorState, enName: true };
+      });
+    }
+    if (!emailRegx.test(formData.email)) {
+      console.log("invalid email");
+      setValidationErrors((prevErrorState) => {
+        return { ...prevErrorState, email: true };
+      });
+    }
+    if (!phoneRegx.test(formData.phoneNumber)) {
+      console.log("invalid number");
+
+      setValidationErrors((prevErrorState) => {
+        return { ...prevErrorState, phoneNumber: true };
+      });
+    }
+    if (!idRegx.test(formData.nationalId)) {
+      console.log("invalid id");
+      setValidationErrors((prevErrorState) => {
+        return { ...prevErrorState, nationalId: true };
+      });
+    }
+  }
+
   // handle typing in fields
   function handleChange(event) {
     const { name, value, maxLength } = event.target;
-    console.log(name)
+    if (!focusedFields.includes(name)) {
+      setFocusedFields((prevFocusedFields) => [...prevFocusedFields, name]);
+    }
     if (
       value.length <= maxLength ||
-      name !== "phoneNumber" &&
-      name !== "nationalId"
+      (name !== "phoneNumber" && name !== "nationalId")
     ) {
       setFormData((prevFormData) => {
         return {
@@ -31,39 +99,20 @@ function App() {
       });
     }
   }
-  // checkvalidation
-  function preValidate(formData) {
-    const teamNameRegx = /\w|^[ء-ي ]*$/;
-    const arNameRegx = /[ء-ي ] [ء-ي ]/g;
-    const enNameRegx = /[a-zA-Z ] [a-zA-Z ]/g;
-    const emailRegx = /\w@(gmail|yahoo).com/g;
-    const phoneRegx = /01(0|1|2|5)\d{8}/g;
-    const idRegx = /(2|3)\d{6}28\d{5}/g;
 
-    if (!teamNameRegx.test(formData.teamName)) {
-      console.log("invalid team name");
-    }
-    if (!arNameRegx.test(formData.arName)) {
-      console.log("invalid arabic name");
-    }
-    if (!enNameRegx.test(formData.enName)) {
-      console.log("invalid english name");
-    }
-    if (!emailRegx.test(formData.email)) {
-      console.log("invalid email");
-    }
-    if (!phoneRegx.test(formData.phoneNumber)) {
-      console.log("invalid number");
-    }
-    if (!phoneRegx.test(formData.phoneNumber)) {
-      console.log("invalid number");
-    }
-  }
+  useEffect(() => {
+    preValidate(formData);
+  }, [formData]);
+
   // handle form submition
   function handleSubmit(event) {
     event.preventDefault();
-    preValidate(formData);
-    console.log(formData);
+    const hasErrors = Object.values(validationErrors).some((error) => error);
+
+    if (!hasErrors) {
+      // this should be the data sent to the server using BackEnd
+      console.log(formData);
+    }
   }
 
   return (
@@ -86,6 +135,11 @@ function App() {
               required
               value={formData.teamName}
               onChange={handleChange}
+              className={
+                validationErrors.teamName && focusedFields.includes("teamName")
+                  ? "error"
+                  : ""
+              }
             ></input>
           </div>
           <div className="grid-item">
@@ -97,6 +151,11 @@ function App() {
               required
               value={formData.arName}
               onChange={handleChange}
+              className={
+                validationErrors.arName && focusedFields.includes("arName")
+                  ? "error"
+                  : ""
+              }
             />
           </div>
           <div className="grid-item">
@@ -108,6 +167,11 @@ function App() {
               required
               value={formData.enName}
               onChange={handleChange}
+              className={
+                validationErrors.enName && focusedFields.includes("enName")
+                  ? "error"
+                  : ""
+              }
             />
           </div>
           <div className="grid-item">
@@ -119,6 +183,11 @@ function App() {
               required
               value={formData.email}
               onChange={handleChange}
+              className={
+                validationErrors.email && focusedFields.includes("email")
+                  ? "error"
+                  : ""
+              }
             />
           </div>
           <div className="grid-item">
@@ -131,6 +200,12 @@ function App() {
               maxLength={11}
               value={formData.phoneNumber}
               onChange={handleChange}
+              className={
+                validationErrors.phoneNumber &&
+                focusedFields.includes("phoneNumber")
+                  ? "error"
+                  : ""
+              }
             />
           </div>
           <div className="grid-item">
@@ -140,10 +215,15 @@ function App() {
               type="number"
               placeholder="اكتب رقمك القومي"
               required
-              // onInput={   }
               maxLength={14}
               value={formData.nationalId}
               onChange={handleChange}
+              className={
+                validationErrors.nationalId &&
+                focusedFields.includes("nationalId")
+                  ? "error"
+                  : ""
+              }
             />
           </div>
           <div className="grid-item">
@@ -153,6 +233,12 @@ function App() {
               required
               value={formData.university}
               onChange={handleChange}
+              className={
+                validationErrors.university &&
+                focusedFields.includes("university")
+                  ? "error"
+                  : ""
+              }
             >
               <option value="">اختر جامعتك</option>
               <option value="أسوان">أسوان</option>
@@ -165,6 +251,12 @@ function App() {
               required
               value={formData.academicYear}
               onChange={handleChange}
+              className={
+                validationErrors.teamName &&
+                focusedFields.includes("academicYear")
+                  ? "error"
+                  : ""
+              }
             >
               <option value="-">اختر سنة دراسية</option>
               <option value="الإعدادية">الإعدادية</option>
